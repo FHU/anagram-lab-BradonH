00c775b84ba4248134905cdb883031e7ad6f690b
BradonH <Bradon.hopper@students.fhu.edu>
2024-02-08 12:06:40 -0600

    update main.py
    test

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(a,b):
+    if len(a) == len(b):
+        for w in range(len(a)):
+            if a[w] in b == True:
+                print('yep')
+                return True
+            else:
+                return False
+    else:
+        return False
+                
 
 if __name__ == '__main__':
     #REMOVE PASS YOUR CODE GOES HERE
-    pass
-    
+    word1 = input()
+    word2 = input()
+    word1.replace(' ', '')
+    word2.replace(' ', '')
+    list1 = []
+    list2 = []
+    for x in range(len(word1)):
+        list1.append(word1[x])
+    for y in range(len(word2)):
+        list2.append(word2[y])
+    anagram(list1, list2)
